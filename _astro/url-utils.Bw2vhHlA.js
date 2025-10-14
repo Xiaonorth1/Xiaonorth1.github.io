47f133e82db076258f11b30d830219f60ccb765f
Xiaonorth1 <170228486+Xiaonorth1@users.noreply.github.com>
2025-10-14 22:34:13 +0000

    Deploying to pages from @ Xiaonorth1/Xiaonorth1.github.io@fd52ae459b71bb4265a7ab666b98f52df936dcad 🚀

---
+++ _astro/url-utils.Bw2vhHlA.js
@@ -0,0 +1,1 @@
+import"./config.8SgpbvHa.js";import"./zh_TW.D-6nPIns.js";function s(o,t){const n=o.replace(/^\/|\/$/g,"").toLowerCase(),r=t.replace(/^\/|\/$/g,"").toLowerCase();return n===r}function e(...o){return o.join("/").replace(/\/+/g,"/")}function c(o){return a(`/posts/${o}/`)}function a(o){return e("","/Xiaonorth1.github.io/",o)}export{c as g,s as p,a as u};
